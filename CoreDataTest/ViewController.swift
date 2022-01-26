ab5613d693f73e57f9b7999a668f6f23cae394ab
Arslan Abdullaev <ars352@yahoo.com>
2022-01-26 20:01:18 +0300

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Arslan Abdullaev on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
